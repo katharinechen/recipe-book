2a6004c2988d3ed1112d74aaeed1ed6b07cf7a14
Katharine Chen & Justin Speers <student@epicodus.com>
2014-08-22 10:27:43 -0700

    User validates presence of name field

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
+  validates :name, presence: true
 
+  def new
+    @user = User.new
+    render('/user/new.html.erb')
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   match('/', :via => :get, :to => 'application#index')
   match('/index', :via => :get, :to => 'application#index')
+
+  match('/user/new', :via => :get, :to => 'user#new')
+  # match('/users:id', :via => :get, :to => 'user#show')
 end
